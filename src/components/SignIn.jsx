115bf58a122cd29edc93f05767935bbb47903475
Jackson <jviana116@gmail.com>
2022-12-29 14:48:24 -0300

    style: estilizando componente SignIn

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,27 @@
 import { auth } from "../firebase";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import "../style/messages.css";
+import { Box } from "@mui/system";
+import { Button } from "@mui/material";
 
 function SingIn() {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
-  return <button onClick={() => signInWithGoogle()}>Sing In with Google</button>;
+  return (
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}
+    >
+      <Button
+        color="inherit"
+        onClick={() => signInWithGoogle()}>
+        <img
+          className="user-image"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoa_4-bNUjFin3_Nx8nBGn8kXrc1UEFBs-Z85XlEeeu6zPLcQ&s"
+          alt="logo do google"
+        />
+        Sing In with Google
+      </Button>
+    </Box>
+  );
 }
 
 export default SingIn;
--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -1,12 +1,26 @@
 import { auth } from "../firebase";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { IconButton } from "@mui/material";
+import { Box } from "@mui/system";
 
 function SignOut() {
   return (
-    <div>
-      {auth.currentUser && (
-        <button onClick={() => auth.signOut()}>Sign Out</button>
-      )}
-    </div>
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Box sx={{ display: "flex" }}>
+        {auth.currentUser && (
+          <IconButton
+            aria-label="logout"
+            color="inherit"
+            onClick={() => auth.signOut()}
+          >
+            <LogoutIcon />
+            LogOut
+          </IconButton>
+        )}
+      </Box>
+    </Box>
   );
 }
 
