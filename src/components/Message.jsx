0a4ff52f966c5ee0da8ffff5f68dfe885a3da448
Jackson <jviana116@gmail.com>
2022-12-29 15:59:41 -0300

    style: componente message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,5 @@
-import { ListItemText } from "@mui/material";
+import { ListItemText, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { Fragment } from "react";
 import "../style/messages.css";
 
@@ -8,22 +9,24 @@ function Message({ message }) {
   // const messageClass = uid === "user" ? "sent" : "received";
 
   return (
-    <Fragment width="100px">
+    <Fragment>
       <img className="user-image" src={photoURL} alt="foto de perfil" />
-      <ListItemText
-        primary={text}
-        sx={{
-          display: "flex",
-          justifyContent: "left",
-          alignItems: "center",
-          width: "100px",
-          height: "30px",
-          backgroundColor: "rgb(0, 162, 255)",
-          color: "white",
-          padding: "5px",
-          borderRadius: "10px",
-        }}
-      />
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        <Typography
+          variant="p"
+          sx={{
+            display: "flex",
+            height: "auto",
+            width: "auto",
+            backgroundColor: "rgb(0, 162, 255)",
+            color: "black",
+            padding: "5px",
+            borderRadius: "10px",
+          }}
+        >
+          {text}
+        </Typography>
+      </Box>
     </Fragment>
   );
 }
